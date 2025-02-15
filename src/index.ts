1ca1d15830aaa6965a54fac4de76444ac73ccfc4
BhargavShekhar <bhargavpathak91021@gmail.com>
2025-02-15 16:33:43 +0530

    online user feature

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,19 @@ const wss = new WebSocketServer({ host: "0.0.0.0", port: 8080 });
 let userCount = 0;
 let allSockets: WebSocket[] = [];
 
+const broadcastUserCount = () => {
+    allSockets.forEach(socket => {
+        socket.send(JSON.stringify({ userCount }));
+    })
+}
+
 wss.on("connection", function (socket) {
     userCount++;
     allSockets.push(socket);
     console.log(`New User Connected, usercount:: #${userCount}`);
+
+    socket.send(JSON.stringify({ userCount })); // for new user connection
+    broadcastUserCount();
 
     socket.on("message", (data) => {
         const { sender, message } = JSON.parse(data.toString());
@@ -24,5 +33,7 @@ wss.on("connection", function (socket) {
         userCount--;
         console.log(`user diconected, usercount ${userCount}`);
         allSockets = allSockets.filter((sockets) => sockets !== socket);
+
+        broadcastUserCount();
     })
 })

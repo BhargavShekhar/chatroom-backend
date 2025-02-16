6a3e3859b4676dc057ba754077b29f86cb547a8b
BhargavShekhar <bhargavpathak91021@gmail.com>
2025-02-16 13:23:50 +0530

    user count bug fix

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,36 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ host: "0.0.0.0", port: 8080 });
 
-let userCount = 0;
 let allSockets: WebSocket[] = [];
 
 const broadcastUserCount = () => {
     allSockets.forEach(socket => {
-        socket.send(JSON.stringify({ userCount }));
+        socket.send(JSON.stringify({ userCount: allSockets.length }));
     })
 }
 
 wss.on("connection", function (socket) {
-    userCount++;
     allSockets.push(socket);
-    console.log(`New User Connected, usercount:: #${userCount}`);
+    console.log(`New User Connected, usercount:: #${allSockets.length}`);
 
-    socket.send(JSON.stringify({ userCount })); // for new user connection
     broadcastUserCount();
 
     socket.on("message", (data) => {
-        const { sender, message } = JSON.parse(data.toString());
-        console.log(`Recieved: ${message} from ${sender}`);
+        const { sender, message, type } = JSON.parse(data.toString());
+        // console.log(`Recieved: ${message} from ${sender}`);
+        if(type === "getUserCount") {
+            socket.send(JSON.stringify({ userCount: allSockets.length }));
+            return;
+        }
         allSockets.forEach(sockets => {
-            if(sockets !== socket) {
+            if (sockets !== socket) {
                 sockets.send(JSON.stringify({ sender, message }));
             }
         })
     })
 
     socket.on("close", () => {
-        userCount--;
-        console.log(`user diconected, usercount ${userCount}`);
+        console.log(`user diconected, usercount ${allSockets.length}`);
         allSockets = allSockets.filter((sockets) => sockets !== socket);
 
         broadcastUserCount();
